6465c0914079668c4312aba46c84fc03d00d8a84
Tyler Michael Smith <tyler@neuralmagic.com>
2025-02-13 18:50:45 +0000

    utils, cleanup
    Signed-off-by: Tyler Michael Smith <tyler@neuralmagic.com>

--- include/cutlass/transform/kernel/sm90_sparse_gemm_compressor.hpp
+++ include/cutlass/transform/kernel/sm90_sparse_gemm_compressor.hpp
@@ -46,6 +46,7 @@
 #include "cutlass/fast_math.h"             // cutlass::ceil_div, cutlass::round_up
 #include "cutlass/kernel_hardware_info.h"  // cutlass::KernelHardwareInfo
 #include "cutlass/numeric_size.h"          // cutlass::bits_to_bytes
+#include "cutlass/numeric_types_traits.hpp" // cutlass::has_negative_zero_v
 #include "cutlass/cuda_host_adapter.hpp"   // cutlass::CudaHostAdapter
 
 namespace cutlass::transform::kernel {
@@ -378,9 +379,11 @@ private:
 
     // Construct a sign bit mask for handling negative zeros 
     ElementAMmaRawUnit sign_mask = { 0 };
-    ElementAMmaRawUnit one_sign_mask = ~(ElementAMmaRawUnit{ 1 } << (cute::sizeof_bits_v<ElementA> - 1));
-    for(int i = 0; i < sizeof(ElementAMmaRawUnit) / sizeof(ElementAUint); ++i) {
-      sign_mask |= one_sign_mask << (i * cute::sizeof_bits_v<ElementA>);
+    if constexpr (has_negative_zero_v<ElementA>) {
+      ElementAMmaRawUnit one_sign_mask = static_cast<ElementAMmaRawUnit>(~(ElementAMmaRawUnit{ 1 } << (cute::sizeof_bits_v<ElementA> - 1)));
+      for(int i = 0; i < sizeof(ElementAMmaRawUnit) / sizeof(ElementAUint); ++i) {
+        sign_mask |= one_sign_mask << (i * cute::sizeof_bits_v<ElementA>);
+      }
     }
 
     // * Compress
@@ -408,7 +411,14 @@ private:
           CUTE_UNROLL
           for (int elt_log_idx = 0; elt_log_idx < OneChunkSizeA{}; ++elt_log_idx) {
             ElementAMmaRawUnit elem_A = tAsA[elt_log_idx];
-            if ( (elem_A & sign_mask) != ElementAMmaRawUnit{0} ) {
+
+            // Handle negative 0
+            ElementAMmaRawUnit masked_elem_A = elem_A;
+            if constexpr (has_negative_zero_v<ElementA>) {
+              masked_elem_A = elem_A & sign_mask;
+            }
+
+            if ( masked_elem_A != ElementAMmaRawUnit{0} ) {
               non_zero_elt_log_idx[non_zero_cnt] = elt_log_idx;
               tACsAC[non_zero_cnt] = elem_A;
               non_zero_cnt++;
