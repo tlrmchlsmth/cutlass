6b06956f57230c5786e9414773fd7d4f2519eea1
Tyler Michael Smith <tyler@neuralmagic.com>
2025-02-13 17:44:56 +0000

    fixup
    Signed-off-by: Tyler Michael Smith <tyler@neuralmagic.com>

--- include/cutlass/transform/kernel/sm90_sparse_gemm_compressor.hpp
+++ include/cutlass/transform/kernel/sm90_sparse_gemm_compressor.hpp
@@ -379,7 +379,7 @@ private:
     // Construct a sign bit mask for handling negative zeros 
     ElementAMmaRawUnit sign_mask = { 0 };
     ElementAUint one_sign_mask = ~(1 << cute::sizeof_bits_v<ElementA>);
-    for(int i = 0; i < sizeof(ElementAMmaRawUnit) / sizeof(ElementAUnit); ++i) {
+    for(int i = 0; i < sizeof(ElementAMmaRawUnit) / sizeof(ElementAUint); ++i) {
       sign_mask |= one_sign_mask << (i * cute::sizeof_bits_v<ElementA>);
     }
 
